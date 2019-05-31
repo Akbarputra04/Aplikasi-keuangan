84353fb415f1ffec12158f6151c259416d8fff32
akbar putra <ap975527@gmail.com>
2019-05-31 08:46:25 +0700

    mengubah warna button yang belum sesuai dan memperbaiki link keluar aplikasi

--- application/views/section/header.php
+++ application/views/section/header.php
@@ -170,3 +170,23 @@
 
         </nav>
         <!-- End of Topbar -->
+
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Keluar aplikasi?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Pilih keluar untuk keluar aplikasi</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Batal</button>
+          <a class="btn btn-success" href="<?= base_url('auth') ?>">keluar</a>
+        </div>
+      </div>
+    </div>
+  </div>
