19f2ed7e86a5e085d31a30d8041c966e2a12f265
akbar putra <ap975527@gmail.com>
2019-08-29 11:58:33 +0700

    menampilkan kategori

---
+++ application/views/kategori.php
@@ -0,0 +1,51 @@
+
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-0">
+        <h1 class="h3 mb-0 text-gray-800">Kategori</h1>
+        </div>
+
+            <!-- DataTales Example -->
+        <div class="card shadow mb-4 mt-4">
+            <div class="card-header d-sm-flex align-items-center justify-content-between py-3">
+                <h6 class="m-0 font-weight-bold text-success">Data kategori</h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($kategori as $key => $value) : ?>
+                            <tr>
+                                <td><?= $key+1 ?></td>
+                                <td><?= $value['nama_kategori'] ?></td>
+                                <td>
+                                <a href="" class="btn btn-warning btn-sm" title="Ubah"><i class="fa fa-edit"></i></a>
+                                <a href="" class="btn btn-danger btn-sm" title="Hapus"><i class="fa fa-trash"></i></a>
+                                </td>
+                            </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+        
+
+    </div>
+    <!-- /.container-fluid -->
